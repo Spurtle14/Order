deae2663be02c127bd6426219ea6a43d9a0c39f5
Spurtle14 <eddes14@gmail.com>
2014-05-21 12:01:24 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+ <title>Protected Page: Please Enter Password</title>
+  <script language="Javascript">
+    var password= prompt("Enter in the password""");
+    {location -"correct.html"}
+    else {location = "error.html"}
+  </script>
